98ce1feb498726fa5c5495d04510de43e6cd0666
David Janda <djanda97@gmail.com>
2019-12-13 01:45:54 -0800

    Added opcode grouping and reverted to running the program for each malware family individually.

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # cs185c-final
-* To install gensim:
+* To install dependencies:
   * python3 -m venv env
   * source env/bin/activate
   * pip3 install -r requirements.txt
-  
- * To install disjointSet for grouping:
-   * pip install disjoint-set
 
-* To run the program:
-  * python3 final.py
+* To run the program (must be run for each malware family):
+  * python3 final.py --family CeeInject
+  * python3 final.py --family Renos
+
+* To compute the embedding vectors for Problem 1c:
+  * python3 final.py --partc
--- final.py
+++ final.py
@@ -3,6 +3,7 @@ from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence, PathLineSentences
 from gensim.models.keyedvectors import WordEmbeddingsKeyedVectors
 from gensim.models.callbacks import CallbackAny2Vec
+from disjoint_set import DisjointSet
 from itertools import combinations
 from pprint import pprint
 import argparse
@@ -11,22 +12,20 @@ import os.path
 import numpy as np
 
 
+class EpochLogger(CallbackAny2Vec):
+    def __init__(self):
+        self.epoch = 1
+
+    def on_epoch_begin(self, model):
+        print("Epoch #{} start".format(self.epoch))
+        self.epoch += 1
+
+
 def get_similarities(word_vectors, opcode_pairs):
     similarities = {}
-    positive_count = 0
-    negative_count = 0
 
     for pair in opcode_pairs:
-        similarity = word_vectors.similarity(pair[0], pair[1])
-        similarities[pair] = similarity
-        if similarity >= 0:
-            positive_count += 1
-        else:
-            negative_count += 1
-
-    print("\npositive_count:", positive_count)
-    print("negative_count:", negative_count)
-    print("difference:", abs(positive_count - negative_count))
+        similarities[pair] = word_vectors.similarity(pair[0], pair[1])
 
     return similarities
 
@@ -46,65 +45,29 @@ def get_top20(filename):
 
 
 def get_opcode_pairs():
-    top20 = get_top20("./cs185c_final_data/top20.txt")
-    opcode_pairs = combinations(top20, 2)
+    if not os.path.exists("opcode_pairs.pickle"):
+        top20 = get_top20("./cs185c_final_data/top20.txt")
+
+        opcode_pairs = combinations(top20, 2)
+
+        with open("opcode_pairs.pickle", "wb") as file:
+            pickle.dump(opcode_pairs, file)
+    else:
+        with open("opcode_pairs.pickle", "rb") as file:
+            opcode_pairs = pickle.load(file)
 
     return opcode_pairs
 
 
-class EpochLogger(CallbackAny2Vec):
-    def __init__(self):
-        self.epoch = 1
-
-    def on_epoch_begin(self, model):
-        print("Epoch #{} start".format(self.epoch))
-        self.epoch += 1
-
-
-def create_model(family):
-    path = "./cs185c_final_data/" + family
-    data = PathLineSentences(path)
-    epoch_logger = EpochLogger()
-
-    model_file = family + ".model"
-
-    if not os.path.exists(model_file):
-        model = Word2Vec(data, size=2, window=6, min_count=1,
-                         workers=4, callbacks=[epoch_logger])
-        model.save(model_file)
-    else:
-        model = Word2Vec.load(model_file)
-
-    word_vectors = model.wv
-
-    opcode_vectors = []
-
-    print("\nopcode_vectors:")
-    for i, opcode in enumerate(word_vectors.vocab):
-        opcode_vectors.append(word_vectors[opcode])
-        print(f"{i + 1}:", opcode, opcode_vectors[i])
-
-    opcode_pairs = get_opcode_pairs()
-
-    similarities = get_similarities(word_vectors, opcode_pairs)
-
-    print("\nsimilarities:")
-    for pair, similarity in similarities.items():
-        print(pair, ":", similarity)
-
-
-def get_opcode_groups():
-    print("Getting opcode groups:")
-
-
-def problem_1a():
-    print("\nProblem 1a:")
-    create_model("CeeInject")
-
-
-def problem_1b():
-    print("\nProblem 1b:")
-    create_model("Renos")
+def print_opcode_groups(similarities):
+    print("\nOpcode grouping based on the computed cosine similarity values:")
+    ds = DisjointSet()
+    for i, j in similarities:
+        ds.find(i)
+        ds.find(j)
+        if similarities[i, j] > 0.85:
+            ds.union(i, j)
+    print(list(ds.itersets()))
 
 
 def create_model_samples(path):
@@ -133,7 +96,7 @@ def get_embedding_vectors(models):
                 embedding_vectors = np.append(embedding_vectors, m.wv[opcode])
             else:
                 embedding_vectors = np.append(embedding_vectors, [0, 0])
-    
+
     embedding_vectors = np.reshape(embedding_vectors, (len(models), 40))
 
     return embedding_vectors
@@ -149,7 +112,7 @@ def print_embedding_vectors(embedding_vectors):
         print(f"\nembedding_vectors[0][{i}]:")
         pprint(embedding_vectors[0][i])
         print(f"embedding_vectors[0][{i}].shape:",
-            embedding_vectors[0][i].shape)
+              embedding_vectors[0][i].shape)
 
 
 def problem_1c():
@@ -179,11 +142,56 @@ def problem_1c():
         np.save(filename, embedding_vectors)
     else:
         embedding_vectors = np.load(filename, allow_pickle=True)
-    
+
     print_embedding_vectors(embedding_vectors)
 
 
+def run(path, family):
+    data = PathLineSentences(path)
+
+    epoch_logger = EpochLogger()
+
+    model_file = family + ".model"
+
+    if not os.path.exists(model_file):
+        model = Word2Vec(data, size=2, window=6, min_count=1,
+                         workers=4, callbacks=[epoch_logger])
+        model.save(model_file)
+    else:
+        model = Word2Vec.load(model_file)
+
+    word_vectors = model.wv
+
+    opcode_vectors = []
+
+    print("\nopcode_vectors:")
+    for i, opcode in enumerate(word_vectors.vocab):
+        opcode_vectors.append(word_vectors[opcode])
+        print(f"{i + 1}:", opcode, opcode_vectors[i])
+
+    opcode_pairs = get_opcode_pairs()
+
+    similarities = get_similarities(word_vectors, opcode_pairs)
+
+    print("\nsimilarities:")
+    for pair, similarity in similarities.items():
+        print(pair, ":", similarity)
+
+    print_opcode_groups(similarities)
+
+
 if __name__ == "__main__":
-    problem_1a()
-    problem_1b()
-    problem_1c()
+    parser = argparse.ArgumentParser(
+        description="Compute embedding vectors for different malware families.")
+    parser.add_argument(
+        "--family", "-f", help="Specify which family of malware to compute embedding vectors for. (CeeInject or Renos)", type=str, default="CeeInject")
+    parser.add_argument(
+        "--partc", "-c", help="Compute the embedding vectors for problem 1c.", action="store_true")
+
+    args = parser.parse_args()
+
+    if args.partc:
+        problem_1c()
+    else:
+        path = "./cs185c_final_data/" + args.family
+        run(path, args.family)
